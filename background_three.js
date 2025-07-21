b70296aafcdebadacb622dafd3b62ff7a37807e2
Khoso74 <balouchp3@gmail.com>
2025-07-21 19:52:23 +0500

    Update background_three.js

--- background_three.js
+++ background_three.js
@@ -1,33 +1,24 @@
-const canvas = document.getElementById('bg-canvas');
+import * as THREE from 'https://cdn.skypack.dev/three';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ canvas });
-
+const renderer = new THREE.WebGLRenderer({canvas: document.getElementById('bg-canvas')});
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Stars
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.2, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const star = new THREE.Mesh(geometry, material);
-
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-  star.position.set(x, y, z);
-  scene.add(star);
-}
-Array(300).fill().forEach(addStar);
-
-// Light
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
-scene.add(pointLight);
-
 camera.position.z = 30;
+
+const geometry = new THREE.SphereGeometry(5, 64, 64);
+const material = new THREE.MeshStandardMaterial({color: 0xffcc00});
+const sun = new THREE.Mesh(geometry, material);
+scene.add(sun);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20, 20, 20);
+scene.add(pointLight);
 
 function animate() {
   requestAnimationFrame(animate);
-  scene.rotation.y += 0.001;
+  sun.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
+
 animate();
