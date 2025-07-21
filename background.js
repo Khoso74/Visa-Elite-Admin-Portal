492c0378f4cc88e8f7dae983b682d12306770dab
Khoso74 <balouchp3@gmail.com>
2025-07-21 11:32:29 +0500

    Update background.js

--- background.js
+++ background.js
@@ -1,20 +1,11 @@
-window.addEventListener('DOMContentLoaded', () => {
-  VANTA.NET({
-    el: "#vanta-bg",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.0,
-    scaleMobile: 1.0,
-    color: 0x00ffff,
-    backgroundColor: 0x0a0a0a
-  });
-
-  // Delay Login Container
-  setTimeout(() => {
-    document.getElementById("login-box").classList.remove("hidden");
-  }, 3500);
+VANTA.NET({
+  el: "#animated-bg",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x007bff,
+  backgroundColor: 0x0d1117,
 });
-
