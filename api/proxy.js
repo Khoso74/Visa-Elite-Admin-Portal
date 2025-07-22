096593091a6d62a3c39811a2877831809dbd46c3
Khoso74 <balouchp3@gmail.com>
2025-07-22 10:43:24 +0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
   if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://khoso74.github.io');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.status(204).end();
@@ -10,14 +10,16 @@ module.exports = async (req, res) => {
   }
 
   const url = 'https://script.google.com/macros/s/AKfycbxIkJL8tNlrZKL2jS2zcfDL3_-XssqRGYWeZvWgbqPTK_pG2FOUSKNYAw-cpgugihdC/exec';
-  
+
   const response = await fetch(url, {
     method: req.method,
-    headers: req.headers,
-    body: req.body,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: req.method === 'POST' ? JSON.stringify(req.body) : null
   });
 
   const data = await response.text();
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://khoso74.github.io');
   res.status(response.status).send(data);
 };
